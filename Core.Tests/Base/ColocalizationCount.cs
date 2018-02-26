a645a55a5b395e19d3531200059905685060e356
VJalili <jalili.vahid@gmail.com>
2018-02-25 16:24:41 -0800

    Add co-localization test for single non-overlapping peak. Also, resolved a bug occurred when raising an exception informing at least two samples are required to run MSPC analysis.

---
+++ Core.Tests/Base/ColocalizationCount.cs
@@ -0,0 +1,43 @@
+﻿using Genometric.GeUtilities.IntervalParsers;
+using Genometric.GeUtilities.IntervalParsers.Model.Defaults;
+using Genometric.MSPC;
+using Genometric.MSPC.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Core.Tests.Base
+{
+    public class ColocalizationCount
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 0)]
+        public void SingleNonOverlappingPeak(byte c, byte expected)
+        {
+            // Arrange
+            var sA = new BED<ChIPSeqPeak>();
+            sA.Add(new ChIPSeqPeak() { Left = 10, Right = 20 }, "chr1", '*');
+
+            var sB = new BED<ChIPSeqPeak>();
+            sB.Add(new ChIPSeqPeak() { Left = 30, Right = 40 }, "chr1", '*');
+
+            var mspc = new MSPC<ChIPSeqPeak>();
+            mspc.AddSample(0, sA);
+            mspc.AddSample(1, sB);
+
+            var config = new Config(ReplicateType.Biological, 1, 1, 1, c, 1F, MultipleIntersections.UseLowestPValue);
+
+            // Act
+            var res = mspc.Run(config);
+
+            // TODO: this step should not be necessary; remove it after the Results class is updated.
+            foreach (var rep in res)
+                rep.Value.ReadOverallStats();
+
+            // Assert
+            Assert.True(res[0].R_j__o["chr1"].Count == expected);
+        }
+    }
+}

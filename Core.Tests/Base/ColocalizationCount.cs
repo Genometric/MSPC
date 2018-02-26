b00882dffa8380d2eded0ed593a2d3e12af785f7
VJalili <jalili.vahid@gmail.com>
2018-02-25 17:18:24 -0800

    Add ThreePeaksTwoOverlapping test.

--- Core.Tests/Base/ColocalizationCount.cs
+++ Core.Tests/Base/ColocalizationCount.cs
@@ -39,5 +39,68 @@ namespace Core.Tests.Base
             // Assert
             Assert.True(res[0].R_j__o["chr1"].Count == expected);
         }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        public void TwoOverlappingPeak(byte c, byte expected)
+        {
+            // Arrange
+            var sA = new BED<ChIPSeqPeak>();
+            sA.Add(new ChIPSeqPeak() { Left = 10, Right = 20 }, "chr1", '*');
+
+            var sB = new BED<ChIPSeqPeak>();
+            sB.Add(new ChIPSeqPeak() { Left = 5, Right = 40 }, "chr1", '*');
+
+            var mspc = new MSPC<ChIPSeqPeak>();
+            mspc.AddSample(0, sA);
+            mspc.AddSample(1, sB);
+
+            var config = new Config(ReplicateType.Biological, 1, 1, 1, c, 1F, MultipleIntersections.UseLowestPValue);
+
+            // Act
+            var res = mspc.Run(config);
+
+            // TODO: this step should not be necessary; remove it after the Results class is updated.
+            foreach (var rep in res)
+                rep.Value.ReadOverallStats();
+
+            // Assert
+            Assert.True(res[0].R_j__o["chr1"].Count == expected);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 1)]
+        public void ThreePeaksTwoOverlapping(byte c, byte expected)
+        {
+            // Arrange
+            var sA = new BED<ChIPSeqPeak>();
+            sA.Add(new ChIPSeqPeak() { Left = 10, Right = 20 }, "chr1", '*');
+
+            var sB = new BED<ChIPSeqPeak>();
+            sB.Add(new ChIPSeqPeak() { Left = 5, Right = 12 }, "chr1", '*');
+
+            var sC = new BED<ChIPSeqPeak>();
+            sC.Add(new ChIPSeqPeak() { Left = 18, Right = 25 }, "chr1", '*');
+
+            var mspc = new MSPC<ChIPSeqPeak>();
+            mspc.AddSample(0, sA);
+            mspc.AddSample(1, sB);
+            mspc.AddSample(2, sC);
+
+            var config = new Config(ReplicateType.Biological, 1, 1, 1, c, 1F, MultipleIntersections.UseLowestPValue);
+
+            // Act
+            var res = mspc.Run(config);
+
+            // TODO: this step should not be necessary; remove it after the Results class is updated.
+            foreach (var rep in res)
+                rep.Value.ReadOverallStats();
+
+            // Assert
+            Assert.True(res[0].R_j__o["chr1"].Count == expected);
+        }
     }
 }

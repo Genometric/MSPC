aaf291bfac74e33d12c008717fa759addec0c4fb
VJalili <jalili.vahid@gmail.com>
2018-02-27 21:57:28 -0800

    Add WeakDiscard tests.

---
+++ Core.Tests/Base/WeakDiscarded.cs
@@ -0,0 +1,64 @@
+﻿using Genometric.GeUtilities.IntervalParsers;
+using Genometric.GeUtilities.IntervalParsers.Model.Defaults;
+using Genometric.MSPC;
+using Genometric.MSPC.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xunit;
+
+namespace Core.Tests.Base
+{
+    public class WeakDiscarded
+    {
+        private ReadOnlyDictionary<uint, AnalysisResult<ChIPSeqPeak>> CreateWeakPeaksAndDiscardThem()
+        {
+            var sA = new BED<ChIPSeqPeak>();
+            sA.Add(new ChIPSeqPeak() { Left = 10, Right = 20, Value = 1e-4 }, "chr1", '*');
+
+            var sB = new BED<ChIPSeqPeak>();
+            sB.Add(new ChIPSeqPeak() { Left = 5, Right = 12, Value = 1e-4 }, "chr1", '*');
+
+            var mspc = new MSPC<ChIPSeqPeak>();
+            mspc.AddSample(0, sA);
+            mspc.AddSample(1, sB);
+
+            var config = new Config(ReplicateType.Biological, 1e-4, 1e-8, 1e-4, 2, 1F, MultipleIntersections.UseLowestPValue);
+
+            // Act
+            var res = mspc.Run(config);
+
+            // TODO: this step should not be necessary; remove it after the Results class is updated.
+            foreach (var rep in res)
+                rep.Value.ReadOverallStats();
+
+            return res;
+        }
+
+        [Fact]
+        public void Separate()
+        {
+            // Arrange & Act
+            var res = CreateWeakPeaksAndDiscardThem();
+
+            // Assert
+            foreach (var s in res)
+                Assert.True(s.Value.R_j__wdt["chr1"] == 1);
+        }
+
+        [Fact]
+        public void Separate2()
+        {
+            // Arrange & Act
+            var res = CreateWeakPeaksAndDiscardThem();
+
+            // Assert
+            foreach (var s in res)
+                Assert.True(s.Value.R_j__c["chr1"].Count == 1);
+            foreach (var s in res)
+                foreach (var p in s.Value.R_j__d["chr1"])
+                    Assert.True(p.Value.classification == PeakClassificationType.WeakDiscarded);
+        }
+    }
+}

f0ee58f382f533b8d40991c14f9d8f709f7d96e2
VJalili <jalili.vahid@gmail.com>
2018-03-03 08:27:41 -0800

    Update Eg1 test to query sets by p-value, as opposed to always considering the first item.

--- Core.Tests/Example/Eg1.cs
+++ Core.Tests/Example/Eg1.cs
@@ -5,6 +5,7 @@ using Genometric.MSPC.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -12,14 +13,14 @@ namespace Core.Tests.Example
 {
     public class Eg1
     {
-        private readonly ChIPSeqPeak r11 = new ChIPSeqPeak() { Left = 3, Right = 13, Name="11", Value = 1e-6 };
-        private readonly ChIPSeqPeak r12 = new ChIPSeqPeak() { Left = 21, Right = 32, Name="12", Value = 1e-12 };
-        private readonly ChIPSeqPeak r21 = new ChIPSeqPeak() { Left = 10, Right = 25, Name="21", Value = 1e-7 };
-        private readonly ChIPSeqPeak r22 = new ChIPSeqPeak() { Left = 30, Right = 37, Name="22", Value = 1e-5 };
-        private readonly ChIPSeqPeak r23 = new ChIPSeqPeak() { Left = 41, Right = 48, Name="23", Value = 1e-6 };
-        private readonly ChIPSeqPeak r31 = new ChIPSeqPeak() { Left = 0, Right = 4, Name="31", Value = 1e-6 };
-        private readonly ChIPSeqPeak r32 = new ChIPSeqPeak() { Left = 8, Right = 17, Name="32", Value = 1e-12 };
-        private readonly ChIPSeqPeak r33 = new ChIPSeqPeak() { Left = 51, Right = 58, Name="33", Value = 1e-18 };
+        private readonly ChIPSeqPeak r11 = new ChIPSeqPeak() { Left = 3, Right = 13, Name = "11", Value = 1e-6 };
+        private readonly ChIPSeqPeak r12 = new ChIPSeqPeak() { Left = 21, Right = 32, Name = "12", Value = 1e-12 };
+        private readonly ChIPSeqPeak r21 = new ChIPSeqPeak() { Left = 10, Right = 25, Name = "21", Value = 1e-7 };
+        private readonly ChIPSeqPeak r22 = new ChIPSeqPeak() { Left = 30, Right = 37, Name = "22", Value = 1e-5 };
+        private readonly ChIPSeqPeak r23 = new ChIPSeqPeak() { Left = 41, Right = 48, Name = "23", Value = 1e-6 };
+        private readonly ChIPSeqPeak r31 = new ChIPSeqPeak() { Left = 0, Right = 4, Name = "31", Value = 1e-6 };
+        private readonly ChIPSeqPeak r32 = new ChIPSeqPeak() { Left = 8, Right = 17, Name = "32", Value = 1e-12 };
+        private readonly ChIPSeqPeak r33 = new ChIPSeqPeak() { Left = 51, Right = 58, Name = "33", Value = 1e-18 };
 
         private MSPC<ChIPSeqPeak> GenerateAndAddEg1Peaks()
         {
@@ -60,22 +61,22 @@ namespace Core.Tests.Example
 
             // Assert
             var s1 = res[0];
-            Assert.True(s1.R_j__d["chr1"][0].peak.CompareTo(r11) == 0 && s1.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
-            Assert.True(s1.R_j__c["chr1"][0].peak.CompareTo(r11) == 0 && s1.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
-            Assert.True(s1.R_j__d["chr1"][1].peak.CompareTo(r12) == 0 && s1.R_j__d["chr1"][1].classification == PeakClassificationType.StringentDiscarded);
-            Assert.True(s1.R_j__c["chr1"][1].peak.CompareTo(r12) == 0 && s1.R_j__c["chr1"][1].classification == PeakClassificationType.StringentConfirmed);
+            Assert.True((s1.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r11) == 0)).Value != null && s1.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
+            Assert.True((s1.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r11) == 0)).Value != null && s1.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
+            Assert.True((s1.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r12) == 0)).Value != null && s1.R_j__d["chr1"][1].classification == PeakClassificationType.StringentDiscarded);
+            Assert.True((s1.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r12) == 0)).Value != null && s1.R_j__c["chr1"][1].classification == PeakClassificationType.StringentConfirmed);
 
             var s2 = res[1];
-            Assert.True(s2.R_j__d["chr1"][0].peak.CompareTo(r21) == 0 && s2.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
-            Assert.True(s2.R_j__d["chr1"][1].peak.CompareTo(r22) == 0 && s2.R_j__d["chr1"][1].classification == PeakClassificationType.WeakDiscarded);
-            Assert.True(s2.R_j__d["chr1"][2].peak.CompareTo(r23) == 0 && s2.R_j__d["chr1"][2].classification == PeakClassificationType.WeakDiscarded);
-            Assert.True(s2.R_j__c["chr1"][0].peak.CompareTo(r21) == 0 && s2.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
+            Assert.True((s2.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r21) == 0)).Value != null && s2.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
+            Assert.True((s2.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r22) == 0)).Value != null && s2.R_j__d["chr1"][1].classification == PeakClassificationType.WeakDiscarded);
+            Assert.True((s2.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r23) == 0)).Value != null && s2.R_j__d["chr1"][2].classification == PeakClassificationType.WeakDiscarded);
+            Assert.True((s2.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r21) == 0)).Value != null && s2.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
             // TODO: check for the count of stringent discarded and stringent confirmed.
 
             var s3 = res[2];
-            Assert.True(s3.R_j__d["chr1"][0].peak.CompareTo(r31) == 0 && s3.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
-            Assert.True(s3.R_j__d["chr1"][1].peak.CompareTo(r33) == 0 && s3.R_j__d["chr1"][1].classification == PeakClassificationType.StringentDiscarded);
-            Assert.True(s3.R_j__c["chr1"][0].peak.CompareTo(r32) == 0 && s3.R_j__c["chr1"][0].classification == PeakClassificationType.StringentConfirmed);
+            Assert.True((s3.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r23) == 0)).Value != null && s3.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
+            Assert.True((s3.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r33) == 0)).Value != null && s3.R_j__d["chr1"][1].classification == PeakClassificationType.StringentDiscarded);
+            Assert.True((s3.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r32) == 0)).Value != null && s3.R_j__c["chr1"][0].classification == PeakClassificationType.StringentConfirmed);
 
             Assert.True(s1.R_j__o["chr1"].Count == 0);
             Assert.True(s2.R_j__o["chr1"].Count == 0);
@@ -99,20 +100,20 @@ namespace Core.Tests.Example
             // Assert
             var s1 = res[0];
             Assert.True(s1.R_j__d["chr1"].Count == 0);
-            Assert.True(s1.R_j__c["chr1"][0].peak.CompareTo(r11) == 0 && s1.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
-            Assert.True(s1.R_j__c["chr1"][1].peak.CompareTo(r12) == 0 && s1.R_j__c["chr1"][1].classification == PeakClassificationType.StringentConfirmed);
+            Assert.True((s1.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r11) == 0)).Value != null && s1.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
+            Assert.True((s1.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r12) == 0)).Value != null && s1.R_j__c["chr1"][1].classification == PeakClassificationType.StringentConfirmed);
 
             var s2 = res[1];
-            Assert.True(s2.R_j__d["chr1"][0].peak.CompareTo(r23) == 0 && s2.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
-            Assert.True(s2.R_j__c["chr1"][0].peak.CompareTo(r21) == 0 && s2.R_j__d["chr1"][1].classification == PeakClassificationType.WeakConfirmed);
-            Assert.True(s2.R_j__c["chr1"][1].peak.CompareTo(r22) == 0 && s2.R_j__d["chr1"][2].classification == PeakClassificationType.WeakConfirmed);
+            Assert.True((s2.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r23) == 0)).Value != null && s2.R_j__d["chr1"][0].classification == PeakClassificationType.WeakDiscarded);
+            Assert.True((s2.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r21) == 0)).Value != null && s2.R_j__d["chr1"][1].classification == PeakClassificationType.WeakConfirmed);
+            Assert.True((s2.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r22) == 0)).Value != null && s2.R_j__d["chr1"][2].classification == PeakClassificationType.WeakConfirmed);
             // TODO: check for the count of stringent discarded and stringent confirmed.
 
             var s3 = res[2];
             Assert.True(s3.total__wdc + s3.total__wdt == 0);
-            Assert.True(s3.R_j__c["chr1"][0].peak.CompareTo(r31) == 0 && s3.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
-            Assert.True(s3.R_j__d["chr1"][0].peak.CompareTo(r33) == 0 && s3.R_j__d["chr1"][0].classification == PeakClassificationType.StringentDiscarded);
-            Assert.True(s3.R_j__c["chr1"][1].peak.CompareTo(r32) == 0 && s3.R_j__c["chr1"][0].classification == PeakClassificationType.StringentConfirmed);
+            Assert.True((s3.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r31) == 0)).Value != null && s3.R_j__c["chr1"][0].classification == PeakClassificationType.WeakConfirmed);
+            Assert.True((s3.R_j__d["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r33) == 0)).Value != null && s3.R_j__d["chr1"][0].classification == PeakClassificationType.StringentDiscarded);
+            Assert.True((s3.R_j__c["chr1"].FirstOrDefault(x => x.Value.peak.CompareTo(r32) == 0)).Value != null && s3.R_j__c["chr1"][0].classification == PeakClassificationType.StringentConfirmed);
 
             Assert.True(s1.R_j__o["chr1"].Count == 2);
             Assert.True(s2.R_j__o["chr1"].Count == 2);

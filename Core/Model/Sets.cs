6137fe831652537d49950ea4040f7fbd7a95753a
VJalili <jalili.vahid@gmail.com>
2018-03-08 18:49:04 -0800

    Remove all total_* properties and `ReadOverallStats` function.

--- Core/Model/Sets.cs
+++ Core/Model/Sets.cs
@@ -66,7 +66,7 @@ namespace Genometric.MSPC.Model
 
         public void Add(I peak, PeakClassificationType type)
         {
-            switch(type)
+            switch (type)
             {
                 case PeakClassificationType.Stringent:
                     R_j__s.Add(peak);
@@ -147,32 +147,6 @@ namespace Genometric.MSPC.Model
 
 
         /// <summary>
-        /// gets total number of stringent peaks.
-        /// </summary>
-        public UInt32 total____s { private set; get; }
-        /// <summary>
-        /// gets total number of weak peaks.
-        /// </summary>
-        public UInt32 total____w { private set; get; }
-        /// <summary>
-        /// gets total number of garbage peaks.
-        /// </summary>
-        public UInt32 total____g { private set; get; }
-        /// <summary>
-        /// gets total number of peaks in output set.
-        /// </summary>
-        public UInt32 total____o { private set; get; }
-
-        /// <summary>
-        /// Total number of False-Positive peaks in output set.
-        /// </summary>
-        public UInt32 total___FP { private set; get; }
-        /// <summary>
-        /// Total number of True-Positive peaks in output set.
-        /// </summary>
-        public UInt32 total___TP { private set; get; }
-
-        /// <summary>
         /// Total number of stringent peaks that are both validated and discarded
         /// through multiple tests.
         /// </summary>
@@ -184,111 +158,6 @@ namespace Genometric.MSPC.Model
         public UInt32 total_wcom { set; get; }
 
 
-
-        /// <summary>
-        /// Total number of Stringent-Confirmed peaks.
-        /// </summary>
-        public UInt32 total___sc { private set; get; }
-        /// <summary>
-        /// Total number of Stringent-Discarded peaks
-        /// failing intersecting regions count test
-        /// </summary>
-        public UInt32 total__sdc { private set; get; }
-        /// <summary>
-        /// Total number of Stringent-Discarded peaks failing x-squared test.
-        /// </summary>
-        public UInt32 total__sdt { private set; get; }
-        /// <summary>
-        /// Total number of Stringent peaks in output set.
-        /// </summary>
-        public UInt32 total___so { private set; get; }
-
-
-        /// <summary>
-        /// Total number of Weak-Confirmed peaks.
-        /// </summary>
-        public UInt32 total___wc { private set; get; }
-        /// <summary>
-        /// Total number of Weak-Discarded peaks
-        /// failing intersecting regions count test.
-        /// </summary>
-        public UInt32 total__wdc { private set; get; }
-        /// <summary>
-        /// Total number of Weak-Discarded peaks failing x-squared test.
-        /// </summary>
-        public UInt32 total__wdt { private set; get; }
-        /// <summary>
-        /// Total number of Weak peaks in Output set.
-        /// </summary>
-        public UInt32 total___wo { private set; get; }
-
-
         public List<string> messages { private set; get; }
-
-        // TODO
-        // THIS MUST NOT BE REQUIRED!
-        public void ReadOverallStats()
-        {
-            total____s = 0;
-            total____w = 0;
-            total____g = 0;
-            total____o = 0;
-
-            total___TP = 0;
-            total___FP = 0;
-
-            total___sc = 0;
-            total__sdc = 0;
-            total__sdt = 0;
-            total___so = 0;
-
-            total___wc = 0;
-            total__wdc = 0;
-            total__wdt = 0;
-            total___wo = 0;
-
-            /*
-            double totalERsCount = 0;
-            foreach (KeyValuePair<string, List<I>> chr in R_j__s)
-            {
-                total____s += (UInt32)R_j__s[chr.Key].Count;
-                total____w += (UInt32)R_j__w[chr.Key].Count;
-                total____g += (UInt32)R_j__b[chr.Key].Count;
-                total____o += (UInt32)R_j__o[chr.Key].Count;
-
-                total___TP += Stats(chr.Key, PeakClassificationType.TruePositive);
-                total___FP += Stats(chr.Key, PeakClassificationType.FalsePositive);
-
-                total___sc += Stats(chr.Key, PeakClassificationType.StringentConfirmed);
-                total__sdc += Stats(chr.Key, PeakClassificationType.StringentDiscardedC);
-                total__sdt += Stats(chr.Key, PeakClassificationType.StringentDiscardedT);
-
-                total___wc += Stats(chr.Key, PeakClassificationType.WeakConfirmed);
-                total__wdc += Stats(chr.Key, PeakClassificationType.WeakDiscardedC);
-                total__wdt += Stats(chr.Key, PeakClassificationType.WeakDiscardedT);
-
-                totalERsCount = R_j__s[chr.Key].Count + R_j__w[chr.Key].Count;
-
-                /*
-                chrwideStats.Add(chr.Key, new ChrWideStat()
-                {
-                    R_j__t_c = (UInt32)totalERsCount,
-                    R_j__s_c = (UInt32)R_j__s[chr.Key].Count,
-                    R_j__s_p = (Math.Round((R_j__s[chr.Key].Count * 100) / totalERsCount, 2)).ToString() + " %",
-                    R_j__w_c = (UInt32)R_j__w[chr.Key].Count,
-                    R_j__w_p = (Math.Round((R_j__w[chr.Key].Count * 100) / totalERsCount, 2)).ToString() + " %",
-                    R_j__c_c = (UInt32)R_j__c[chr.Key].Count,
-                    R_j__c_p = (Math.Round((R_j__c[chr.Key].Count * 100) / totalERsCount, 2)).ToString() + " %",
-                    R_j__d_c = (UInt32)R_j__d[chr.Key].Count,
-                    R_j__d_p = (Math.Round((R_j__d[chr.Key].Count * 100) / totalERsCount, 2)).ToString() + " %",
-                    R_j__o_c = (UInt32)R_j__o[chr.Key].Count,
-                    R_j__o_p = (Math.Round((R_j__o[chr.Key].Count * 100) / totalERsCount, 2)).ToString() + " %",
-                    R_j_TP_c = Stats(chr.Key, PeakClassificationType.TruePositive),
-                    R_j_TP_p = (Math.Round(((double)Stats(chr.Key, PeakClassificationType.TruePositive) * 100) / (double)R_j__o[chr.Key].Count, 2)).ToString() + " %",
-                    R_j_FP_c = Stats(chr.Key, PeakClassificationType.FalsePositive),
-                    R_j_FP_p = (Math.Round(((double)Stats(chr.Key, PeakClassificationType.FalsePositive) * 100) / (double)R_j__o[chr.Key].Count, 2)).ToString() + " %"
-                });*/
-            //}
-        }
     }
 }

2d0767e156f36ff58e43d40750245a5d915a11fa
VJalili <jalili.vahid@gmail.com>
2018-05-19 15:02:38 -0700

    Add a white-space to copyright header.

--- Core/Model/Enums.cs
+++ Core/Model/Enums.cs
@@ -6,7 +6,7 @@
  * MSPC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
-**/
+ **/
 
 namespace Genometric.MSPC.Model
 {

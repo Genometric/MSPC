a7f4ef122d58d66bbfc4638ca0cd0b7a5afcbf91
VJalili <jalili.vahid@gmail.com>
2018-05-19 14:56:31 -0700

    Remove unused usings.

--- Core/Model/Enums.cs
+++ Core/Model/Enums.cs
@@ -1,9 +1,4 @@
-﻿
-
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-/** Copyright © 2014-2015 Vahid Jalili
+﻿/** Copyright © 2014-2015 Vahid Jalili
 * 
 * This file is part of MSPC project.
 * MSPC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -12,6 +7,7 @@ using System.Linq;
 * PARTICULAR PURPOSE. See the GNU General Public License for more details.
 * You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
 **/
+
 namespace Genometric.MSPC.Model
 {
     public enum Attributes : byte

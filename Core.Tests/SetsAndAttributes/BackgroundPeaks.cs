7c41078eff417fcea07e0e9ec7ca05d2206ad38d
VJalili <jalili.vahid@gmail.com>
2018-05-25 23:09:12 -0700

    Assert Background peak overlapping non-background peaks.

--- Core.Tests/SetsAndAttributes/BackgroundPeaks.cs
+++ Core.Tests/SetsAndAttributes/BackgroundPeaks.cs
@@ -87,5 +87,29 @@ namespace Core.Tests.Base
             foreach(var s in res)
                 Assert.True(s.Value.Chromosomes[_chr].GetInitialClassifications(Attributes.Background).Count == 1);
         }
+
+        [Fact]
+        public void BackgroundOverlappingNonBackground()
+        {
+            // Arrange
+            var sA = new BED<ChIPSeqPeak>();
+            sA.Add(new ChIPSeqPeak() { Left = 10, Right = 20, Value = 1e-2 }, _chr, _strand);
+
+            var sB = new BED<ChIPSeqPeak>();
+            sB.Add(new ChIPSeqPeak() { Left = 50, Right = 60, Value = 1e-8 }, _chr, _strand);
+
+            var mspc = new MSPC<ChIPSeqPeak>();
+            mspc.AddSample(0, sA);
+            mspc.AddSample(1, sB);
+
+            var config = new Config(ReplicateType.Biological, 1e-4, 1e-8, 1e-4, 2, 1F, MultipleIntersections.UseLowestPValue);
+
+            // Act
+            var res = mspc.Run(config);
+
+            Assert.True(
+                res[0].Chromosomes[_chr].GetInitialClassifications(Attributes.Background).Count == 1 &&
+                res[1].Chromosomes[_chr].GetInitialClassifications(Attributes.Background).Count == 0);
+        }
     }
 }

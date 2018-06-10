f1eb0eb7d170caf7b65e3964ddf4b2bd42fbd637
VJalili <jalili.vahid@gmail.com>
2018-06-09 19:42:51 -0700

    Rewrite CommandLineOptions class leveraging Extensions.CommandLineUtils instead of CommandLineParser library.

--- CLI/CommandLineOptions.cs
+++ CLI/CommandLineOptions.cs
@@ -2,50 +2,105 @@
 // The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
 // See the LICENSE file in the project root for more information.
 
-//using CommandLine;
-//using CommandLine.Text;
+using Genometric.MSPC.Model;
+using Microsoft.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 
 namespace Genometric.MSPC.CLI
 {
-    /*
     internal class CommandLineOptions
     {
-        [OptionList('i', "input", Separator = '&', Required = true, HelpText = "Input samples to be processed in Browser Extensible Data (BED) Format")]
-        public IList<string> inputFiles { set; get; }
+        private readonly CommandLineApplication _cla;
 
-        [Option('r', "replicate", Required = true, /*DefaultValue = "Biological", HelpText = "Sets the replicate type of samples. Possible values are: { Bio, Biological, Tec, Technical }")]
-        public string replicateType { set; get; }
-
-        [Option('s', "tauS", Required = true, /*DefaultValue = 1E-8, HelpText = "Sets stringency threshold. All peaks with p-values lower than this value are considered as stringent peaks")]
-        public double tauS { set; get; }
-
-        [Option('w', "tauW", Required = true, /*DefaultValue = 1E-4, HelpText = "Sets weak threshold. All peaks with p-values higher than this value are considered as weak peaks.")]
-        public double tauW { set; get; }
-
-        [Option('g', "gamma", Required = false, DefaultValue = -1, HelpText = "Sets combined stringency threshold. The peaks with their combined p-values satisfying this threshold will be confirmed.")]
-        public double gamma { set; get; }
-
-        [Option('a', "alpha", Required = false, DefaultValue = 0.05F, HelpText = "Sets false discovery rate of Benjamini–Hochberg step-up procedure")]
-        public float alpha { set; get; }
-
-        [Option('c', "C", Required = false, DefaultValue = (byte)1, HelpText = "Sets minimum number of required regions for a valid intersecting group.")]
-        public byte C { set; get; }
-
-        [Option('m', "multipleIntersections", Required = false, DefaultValue = "Lowest", HelpText = "Sets the default peak to be used when multiple peaks from one sample intesect with a given peak. Possible values are: { Lowest, Highest }")]
-        public string M { set; get; }
-
-
-        [ParserState]
-        public IParserState LastParserState { get; set; }
-
-        [HelpOption]
-        public string GetUsage()
+        private static CommandOption _input = new CommandOption("-i | --input <value>", CommandOptionType.MultipleValue)
         {
-            return HelpText.AutoBuild(this,
-              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            Description = "Input samples to be processed in Browser Extensible Data (BED) Format."
+        };
+
+        private static CommandOption _replicate = new CommandOption("-r | --replicate <value>", CommandOptionType.SingleValue)
+        {
+            Description = "Sets the replicate type of samples. Possible values are: { Bio, Biological, Tec, Technical }"
+        };
+
+        private static CommandOption _tauS = new CommandOption("-s | --tauS <value>", CommandOptionType.SingleValue)
+        {
+            Description = "Sets stringency threshold. All peaks with p-values lower than this value are considered as stringent peaks."
+        };
+
+        private static CommandOption _tauW = new CommandOption("-w | --tauW <value>", CommandOptionType.SingleValue)
+        {
+            Description = "Sets weak threshold. All peaks with p-values higher than this value are considered as weak peaks."
+        };
+
+        private static CommandOption _gamma = new CommandOption("-g | --gamma <value>", CommandOptionType.SingleValue)
+        {
+            Description = "Sets combined stringency threshold. The peaks with their combined p-values satisfying this threshold will be confirmed."
+        };
+
+        private static CommandOption _alpha = new CommandOption("-a | --alpha <value>", CommandOptionType.SingleValue)
+        {
+            Description = "Sets false discovery rate of Benjamini–Hochberg step-up procedure."
+        };
+
+        private static CommandOption _c = new CommandOption("-c <value>", CommandOptionType.SingleValue)
+        {
+            Description = "Sets minimum number of overlapping peaks before combining p-values."
+        };
+
+        private static CommandOption _m = new CommandOption("-m | --multipleIntersections <value>", CommandOptionType.SingleValue)
+        {
+            Description = "When multiple peaks from a sample overlap with a given peak, " +
+                "this argument defines which of the peaks to be considered: the one with lowest p-value, or " +
+                "the one with highest p-value? Possible values are: { Lowest, Highest }"
+        };
+
+        private ReplicateType _vreplicate;
+        private double _vtauS = 1E-8;
+        private double _vtauW = 1E-4;
+        private double _vgamma = -1;
+        private float _valpha = 0.05F;
+        private byte _vc = 1;
+        private MultipleIntersections _vm = MultipleIntersections.UseLowestPValue;
+
+
+        public Config Options { private set; get; }
+
+        public CommandLineOptions()
+        {
+            _cla = new CommandLineApplication();
+            _cla.Options.Add(_replicate);
+            _cla.Options.Add(_tauS);
+            _cla.Options.Add(_tauW);
+            _cla.Options.Add(_gamma);
+            _cla.Options.Add(_alpha);
+            _cla.Options.Add(_c);
+            _cla.Options.Add(_m);
+            Func<int> assertArguments = AssertArguments;
+            _cla.OnExecute(assertArguments);
+        }
+
+        private static int AssertArguments()
+        {
+            var missingArgs = new List<string>();
+            if (!_replicate.HasValue()) missingArgs.Add(_replicate.ShortName + "|" + _replicate.LongName);
+            if (!_tauS.HasValue()) missingArgs.Add(_tauS.ShortName + "|" + _tauS.LongName);
+            if (!_tauW.HasValue()) missingArgs.Add(_tauW.ShortName + "|" + _tauW.LongName);
+            
+            // TODO: throw a more informative exception.
+            if (missingArgs.Count > 0)
+                throw new Exception();
+
+            // TODO: check arguments value type. 
+
+            return 0;
+        }
+
+        private Config Parse(string[] args)
+        {
+            _cla.Execute(args);
+            Options = new Config(_vreplicate, _vtauW, _vtauS, _vgamma, _vc, _valpha, _vm);
+            return Options;
         }
     }
-
-*/
 }

82a45f086de911cfe997ec8b81fdd1514e81bcc5
VJalili <jalili.vahid@gmail.com>
2018-06-18 23:21:41 -0700

    Remove unused using in CLI tests class.

--- CLI.Tests/Main.cs
+++ CLI.Tests/Main.cs
@@ -2,10 +2,8 @@
 // The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
 // See the LICENSE file in the project root for more information.
 
-using Genometric.MSPC.Model;
 using System;
 using System.IO;
-using System.Text;
 using Xunit;
 
 namespace Genometric.MSPC.CLI.Tests

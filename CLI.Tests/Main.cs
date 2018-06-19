8a778af9ba7972baafb0f1a473a2da31b1313eca
VJalili <jalili.vahid@gmail.com>
2018-06-18 22:50:23 -0700

    Assert successful analysis of two dummy samples; only from the CLI perspective.

--- CLI.Tests/Main.cs
+++ CLI.Tests/Main.cs
@@ -53,5 +53,32 @@ namespace Genometric.MSPC.CLI.Tests
                 Assert.Equal("Missing file: rep1.bed\r\nMSPC cannot continue.\r\n", sw.ToString());
             }
         }
+
+        [Fact]
+        public void SuccessfulAnalysis()
+        {
+            // Arrange
+            var rep1 = Path.GetTempPath() + Guid.NewGuid().ToString() + ".bed";
+            FileStream stream = File.Create(rep1);
+            using (StreamWriter writter = new StreamWriter(stream))
+                writter.WriteLine("chr1\t10\t20\tmspc_peak_1\t100.0");
+
+            var rep2 = Path.GetTempPath() + Guid.NewGuid().ToString() + ".bed";
+            stream = File.Create(rep2);
+            using (StreamWriter writter = new StreamWriter(stream))
+                writter.WriteLine("chr1\t8\t22\tmspc_peak_2\t110.0");
+
+            // Act
+            string msg;
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Program.Main(String.Format("-i {0} -i {1} -r bio -w 1E-2 -s 1E-8", rep1, rep2).Split(' '));
+                msg = sw.ToString();
+            }
+
+            // Assert
+            Assert.Contains("All processes successfully finished [Analysis ET: ", msg);
+        }
     }
 }

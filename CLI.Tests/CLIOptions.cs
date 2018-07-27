5967cfb6cc115f14874dd97c6422b1b2ad7efdb3
VJalili <jalili.vahid@gmail.com>
2018-07-27 00:58:15 -0700

    Assert error message for the invalid tauW value.

--- CLI.Tests/CLIOptions.cs
+++ CLI.Tests/CLIOptions.cs
@@ -241,7 +241,8 @@ namespace Genometric.MSPC.CLI.Tests
             string[] arguments = "-i rep1.bed -i rep2.bed -w ABC -s 1E-8 -r bio".Split(' ');
 
             // Assert
-            Assert.Throws<ArgumentException>(() => options.Parse(arguments));
+            var exception = Assert.Throws<ArgumentException>(() => options.Parse(arguments));
+            Assert.Equal("Invalid value given for the tauW argument.", exception.Message);
         }
 
         [Fact]

2de98a2c6ac538ac7f8d5a6a967f19b4c2c2cf2d
VJalili <jalili.vahid@gmail.com>
2018-07-31 23:16:18 -0700

    Assert the message of Reason attribut of a peak discarded for insufficient C.

--- Core.Tests/Basic/PeakReason.cs
+++ Core.Tests/Basic/PeakReason.cs
@@ -67,5 +67,28 @@ namespace Genometric.MSPC.Core.Tests.Basic
             // Assert
             Assert.True(res[0].Chromosomes[_chr].Get(Attributes.Discarded).First().Reason == "X-squared is below chi-squared of Gamma.");
         }
+
+        [Fact]
+        public void MessageOfDiscardedForC()
+        {
+            // Arrange
+            var sA = new BED<ChIPSeqPeak>();
+            sA.Add(new ChIPSeqPeak() { Left = 10, Right = 20, Value = 1E-5, HashKey = 1 }, _chr, _strand);
+
+            var sB = new BED<ChIPSeqPeak>();
+            sB.Add(new ChIPSeqPeak() { Left = 22, Right = 28, Value = 1E-5, HashKey = 2 }, _chr, _strand);
+
+            var mspc = new MSPC<ChIPSeqPeak>();
+            mspc.AddSample(0, sA);
+            mspc.AddSample(1, sB);
+
+            var config = new Config(ReplicateType.Biological, 1E-2, 1E-4, 1E-50, 2, 1F, MultipleIntersections.UseLowestPValue);
+
+            // Act
+            var res = mspc.Run(config);
+
+            // Assert
+            Assert.True(res[0].Chromosomes[_chr].Get(Attributes.Discarded).First().Reason == "Intersecting peaks count doesn't comply minimum C requirement.");
+        }
     }
 }

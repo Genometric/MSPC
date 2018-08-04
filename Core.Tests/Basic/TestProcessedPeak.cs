01f872daaae6a90b0e6fd4c1d1db6942cbc2b25a
VJalili <jalili.vahid@gmail.com>
2018-08-04 12:47:09 -0700

    Implement a unit test for the smaller or equal operator of the ProcessedPeak class.

--- Core.Tests/Basic/TestProcessedPeak.cs
+++ Core.Tests/Basic/TestProcessedPeak.cs
@@ -143,5 +143,22 @@ namespace Genometric.MSPC.Core.Tests.Basic
             // Assert
             Assert.True(r == expectedResult);
         }
+
+        [Theory]
+        [InlineData(100, 10, false)]
+        [InlineData(10, 100, true)]
+        [InlineData(100, 100, true)]
+        public void SmallerOrEqualOperator(int xValue, int yValue, bool expectedResult)
+        {
+            // Arrange
+            _x.Source.Value = xValue;
+            _y.Source.Value = yValue;
+
+            // Act
+            var r = _x <= _y;
+
+            // Assert
+            Assert.True(r == expectedResult);
+        }
     }
 }

f0ff496e05c56cb7871abf247f945a01fa1355b5
VJalili <jalili.vahid@gmail.com>
2018-08-04 18:01:13 -0700

    Remove some never-used functions from the IntervalOperations class, and move its used functions to the Node class.

--- Core/IntervalTree/Node.cs
+++ Core/IntervalTree/Node.cs
@@ -2,11 +2,8 @@
 // The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Genometric.GeUtilities.IGenomics;
+using System.Collections.Generic;
 
 namespace Genometric.MSPC.IntervalTree
 {
@@ -79,106 +76,23 @@ namespace Genometric.MSPC.IntervalTree
             return null;
         }
 
-        private List<I> GetIntervalsOfKeys(List<I> intervalKeys)
-        {
-            var allIntervals =
-              from k in intervalKeys
-              select _intervals[k];
-
-            return allIntervals.SelectMany(x => x).ToList();
-        }
-
-        public IEnumerable<IList<I>> Intersections
-        {
-            get
-            {
-                if (_intervals.Count == 0) yield break;
-                else if (_intervals.Count == 1)
-                {
-                    if (_intervals.First().Value.Count > 1)
-                    {
-                        yield return _intervals.First().Value;
-                    }
-                }
-                else
-                {
-                    var keys = _intervals.Keys.ToArray();
-
-                    int lastIntervalIndex = 0;
-                    var intersectionsKeys = new List<I>();
-                    for (int index = 1; index < _intervals.Count; index++)
-                    {
-                        var intervalKey = keys[index];
-                        if (IntervalOperations<I>.Intersects(intervalKey, keys[lastIntervalIndex]))
-                        {
-                            if (intersectionsKeys.Count == 0)
-                            {
-                                intersectionsKeys.Add(keys[lastIntervalIndex]);
-                            }
-                            intersectionsKeys.Add(intervalKey);
-                        }
-                        else
-                        {
-                            if (intersectionsKeys.Count > 0)
-                            {
-                                yield return GetIntervalsOfKeys(intersectionsKeys);
-                                intersectionsKeys = new List<I>();
-                                index--;
-                            }
-                            else
-                            {
-                                if (_intervals[intervalKey].Count > 1)
-                                {
-                                    yield return _intervals[intervalKey];
-                                }
-                            }
-
-                            lastIntervalIndex = index;
-                        }
-                    }
-
-                    if (intersectionsKeys.Count > 0) yield return GetIntervalsOfKeys(intersectionsKeys);
-                }
-            }
-        }
-
         public List<I> Query(I target)
         {
             List<I> result = new List<I>();
 
             foreach (var entry in _intervals)
-            {
-                if (IntervalOperations<I>.Intersects(entry.Key, target))
+                if (target.Right.CompareTo(entry.Key.Left) > 0 &&
+                    target.Left.CompareTo(entry.Key.Right) < 0)
                     foreach (I interval in entry.Value)
                         result.Add(interval);
                 else if (entry.Key.Left.CompareTo(target.Right) > 0)
                     break;
-            }
 
             if (target.Left.CompareTo(_center) < 0 && _leftNode != null)
                 result.AddRange(_leftNode.Query(target));
             if (target.Right.CompareTo(_center) > 0 && _rightNode != null)
                 result.AddRange(_rightNode.Query(target));
             return result;
-        }
-
-
-        public int Center
-        {
-            get { return _center; }
-            set { _center = value; }
-        }
-
-        public Node<I> Left
-        {
-            get { return _leftNode; }
-            set { _leftNode = value; }
-        }
-
-        public Node<I> Right
-        {
-            get { return _rightNode; }
-            set { _rightNode = value; }
         }
     }
 }

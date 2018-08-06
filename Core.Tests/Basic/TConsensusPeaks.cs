bdec0004aaad1e94075e6c194343388380af8c40
VJalili <jalili.vahid@gmail.com>
2018-08-05 23:39:53 -0700

    Update unit tests of the consensus peaks.

--- Core.Tests/Basic/TConsensusPeaks.cs
+++ Core.Tests/Basic/TConsensusPeaks.cs
@@ -12,17 +12,22 @@ namespace Genometric.MSPC.Core.Tests.Basic
 {
     public class TConsensusPeaks
     {
-        [Fact]
-        public void MergeTwoConsensusPeaks()
+        [Theory]
+        [InlineData(10, 20, 5, 15, 5, 20)]
+        [InlineData(10, 20, 12, 18, 10, 20)]
+        [InlineData(10, 20, 5, 25, 5, 25)]
+        [InlineData(10, 20, 19, 25, 10, 25)]
+        [InlineData(19, 25, 10, 20, 10, 25)]
+        public void MergeTwoConsensusPeaks(int xLeft, int xRight, int yLeft, int yRight, int cLeft, int cRight)
         {
             // Arrange
             var mspc = new MSPC<ChIPSeqPeak>();
             var sA = new BED<ChIPSeqPeak>();
-            sA.Add(new ChIPSeqPeak() { Left = 10, Right = 20 }, "chr1", '*');
+            sA.Add(new ChIPSeqPeak() { Left = xLeft, Right = xRight }, "chr1", '*');
             mspc.AddSample(0, sA);
 
             var sB = new BED<ChIPSeqPeak>();
-            sB.Add(new ChIPSeqPeak() { Left = 5, Right = 15 }, "chr1", '*');
+            sB.Add(new ChIPSeqPeak() { Left = yLeft, Right = yRight }, "chr1", '*');
             mspc.AddSample(1, sB);
 
             // Act
@@ -30,7 +35,7 @@ namespace Genometric.MSPC.Core.Tests.Basic
             var cp = mspc.GetMergedReplicates()["chr1"].First().Value;
 
             // Assert
-            Assert.True(cp.Left == 5 && cp.Right == 20);
+            Assert.True(cp.Left == cLeft && cp.Right == cRight);
         }
     }
 }

a83bddd304fb334dd0ce93b8d523acb5cd9995eb
VJalili <jalili.vahid@gmail.com>
2018-08-07 23:14:06 -0700

    Remove unused usings.

--- Core/IntervalTree/Tree.cs
+++ Core/IntervalTree/Tree.cs
@@ -2,10 +2,8 @@
 // The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Genometric.GeUtilities.IGenomics;
+using System.Collections.Generic;
 
 namespace Genometric.MSPC.IntervalTree
 {

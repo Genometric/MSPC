6b074c50a079c30702cc3d31ea1ea2532fdcc926
VJalili <jalili.vahid@gmail.com>
2018-08-12 00:32:18 -0700

    Add unit test for concurrency.

---
+++ Core.Tests/Concurrency/Concurrency.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+using Genometric.GeUtilities.IntervalParsers;
+using Genometric.GeUtilities.IntervalParsers.Model.Defaults;
+using Genometric.MSPC.Model;
+using System.Linq;
+using Xunit;
+
+namespace Genometric.MSPC.Core.Tests.Concurrency
+{
+    public class Concurrency
+    {
+        private BED<ChIPSeqPeak> CreateSample(int offset, int chrCount, int iCount)
+        {
+            var rtv = new BED<ChIPSeqPeak>();
+            for (int c = 0; c < chrCount; c++)
+                for (int i = 0; i < iCount; i++)
+                    rtv.Add(
+                        new ChIPSeqPeak()
+                        {
+                            Left = (10 * i) + 1 + offset,
+                            Right = (10 * i) + 4 + offset,
+                            Value = 1E-6,
+                            Name = "r1" + i,
+                            HashKey = (uint)i
+                        },
+                        "chr" + c,
+                        '*');
+
+            return rtv;
+        }
+
+        [Fact]
+        public void NumberOfPeaks()
+        {
+            // Arrange
+            var mspc = new MSPC<ChIPSeqPeak>();
+            mspc.AddSample(0, CreateSample(0, 20, 1000));
+            mspc.AddSample(1, CreateSample(2, 20, 2000));
+
+            // Act
+            var res = mspc.Run(new Config(ReplicateType.Biological, 1e-4, 1e-5, 1e-5, 2, 0.05F, MultipleIntersections.UseLowestPValue));
+
+            // Assert
+            for(int c=0;c<20;c++)
+            {
+                Assert.True(res[0].Chromosomes["chr" + c].Get(Attributes.Confirmed).Count() == 1000);
+                Assert.True(res[1].Chromosomes["chr" + c].Get(Attributes.Confirmed).Count() == 2000);
+            }
+        }
+    }
+}

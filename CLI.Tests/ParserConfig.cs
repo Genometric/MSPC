21d17db8ee90568b76a5ed9d7b820cd4bd2a6ea2
vjalili <jalili.vahid@gmail.com>
2018-09-24 20:14:56 -0700

    Add a json parser for ParserConfig and its unit test.

---
+++ CLI.Tests/ParserConfig.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+using Genometric.GeUtilities.Intervals.Parsers.Model;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Genometric.MSPC.CLI.Tests
+{
+    public class ParserConfig
+    {
+        private bool Equal(BedColumns obj1, BedColumns obj2)
+        {
+            return
+                obj1.Chr == obj2.Chr &&
+                obj1.Left == obj2.Left &&
+                obj1.Right == obj2.Right &&
+                obj1.Name == obj2.Name &&
+                obj1.Strand == obj2.Strand &&
+                obj1.Summit == obj2.Summit;
+        }
+
+        [Fact]
+        public void ReadParserConfig()
+        {
+            // Arrange
+            var cols = new BedColumns();
+            var path = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "MSPCTests_" + new Random().NextDouble().ToString();
+            var obj = JsonConvert.SerializeObject(cols);
+            using (StreamWriter w = new StreamWriter(path))
+                w.WriteLine(obj);
+
+            // Act
+            var parsedCols = new CLI.ParserConfig().ParseBed(path);
+            File.Delete(path);
+
+            Assert.True(Equal(parsedCols, cols));
+        }
+    }
+}
---
+++ CLI/ParserConfig.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the Genometric organization (https://github.com/Genometric) under one or more agreements.
+// The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
+// See the LICENSE file in the project root for more information.
+
+using Genometric.GeUtilities.Intervals.Parsers.Model;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Genometric.MSPC.CLI
+{
+    internal class ParserConfig
+    {
+        public BedColumns ParseBed(string path)
+        {
+            string json = null;
+            using (StreamReader r = new StreamReader(path))
+                json = r.ReadToEnd();
+
+            return JsonConvert.DeserializeObject<BedColumns>(json);
+        }
+    }
+}

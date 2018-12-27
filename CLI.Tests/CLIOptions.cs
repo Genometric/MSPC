fa06e63ae61c9d5861abcc7298ab178182b17801
VJalili <Jalili.vahid@gmail.com>
2018-12-27 12:13:56 -0800

    Add unit test asserting throwing exception for invalid % number given as the C argument.

--- CLI.Tests/CLIOptions.cs
+++ CLI.Tests/CLIOptions.cs
@@ -352,6 +352,18 @@ namespace Genometric.MSPC.CLI.Tests
         }
 
         [Fact]
+        public void ThrowExceptionForInvalidPercentageOfC()
+        {
+            // Arrange & Act
+            var options = new CommandLineOptions();
+            string[] arguments = "-i rep1.bed -i rep2.bed -w 1E-2 -s 1e-8 -r bio -c 1A%".Split(' ');
+
+            // Assert
+            var exception = Assert.Throws<ArgumentException>(() => options.Parse(arguments));
+            Assert.Equal("Invalid value given for the `c` argument.", exception.Message);
+        }
+
+        [Fact]
         public void ThrowExceptionForInvalidMultipleIntersection()
         {
             // Arrange & Act

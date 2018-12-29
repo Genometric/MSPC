e451e9a16df8c54fe715f6437209d31a7e68c855
VJalili <Jalili.vahid@gmail.com>
2018-12-29 11:12:44 -0800

    Remove an unused using.

--- CLI.Tests/ParserConfig.cs
+++ CLI.Tests/ParserConfig.cs
@@ -2,7 +2,6 @@
 // The Genometric organization licenses this file to you under the GNU General Public License v3.0 (GPLv3).
 // See the LICENSE file in the project root for more information.
 
-using Genometric.GeUtilities.Intervals.Parsers.Model;
 using Newtonsoft.Json;
 using System;
 using System.IO;

d2c444b68d5425acd3bfbf61f8db2c0dc6a1ce15
VJalili <Jalili.vahid@gmail.com>
2018-12-31 13:51:43 -0800

    Add a unit test to assert showing help text.

--- CLI.Tests/Main.cs
+++ CLI.Tests/Main.cs
@@ -34,12 +34,15 @@ namespace Genometric.MSPC.CLI.Tests
             }
         }
 
-        private string RunMSPC(string rep1, string rep2)
+        private string RunMSPC(string rep1 = null, string rep2 = null, string template = null)
         {
             using (StringWriter sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Program.Main(string.Format("-i {0} -i {1} -r bio -w 1E-2 -s 1E-8", rep1, rep2).Split(' '));
+                if (template != null)
+                    Program.Main(template.Split(' '));
+                else
+                    Program.Main(string.Format("-i {0} -i {1} -r bio -w 1E-2 -s 1E-8", rep1, rep2).Split(' '));
                 return sw.ToString();
             }
         }
@@ -247,6 +250,34 @@ namespace Genometric.MSPC.CLI.Tests
 
             // Assert
             Assert.True(Regex.Matches(consoleOutput, "Read peaks#:	2").Count == 2);
+        }
+
+        [Theory]
+        [InlineData("-?")]
+        [InlineData("-h")]
+        [InlineData("--help")]
+        public void ShowsHelpText(string template)
+        {
+            // Arrange
+            string expected = 
+                "\r\n\r\nUsage:  [options]" +
+                "\r\n\r\nOptions:" +
+                "\r\n -? | -h | --help                      Show help information" +
+                "\r\n - i | --input < value > Input samples to be processed in Browser Extensible Data(BED) Format." +
+                "\r\n - p | --parser < value > Sets the path to the parser configuration file in JSON." +
+                "\r\n - r | --replicate < value > Sets the replicate type of samples.Possible values are: { Bio, Biological, Tec, Technical }" +
+                "\r\n - s | --tauS < value > Sets stringency threshold. All peaks with p-values lower than this value are considered as stringent peaks." +
+                "\r\n - w | --tauW < value > Sets weak threshold. All peaks with p-values higher than this value are considered as weak peaks." +
+                "\r\n - g | --gamma < value > Sets combined stringency threshold.The peaks with their combined p-values satisfying this threshold will be confirmed." +
+                "\r\n - a | --alpha < value > Sets false discovery rate of Benjamini–Hochberg step-up procedure.\r\n - c < value > Sets minimum number of overlapping peaks before combining p-values." +
+                "\r\n - m | --multipleIntersections < value > When multiple peaks from a sample overlap with a given peak, this argument defines which of the peaks to be considered: the one with lowest p - value, or the one with highest p-value ? Possible values are: { Lowest, Highest }" +
+                "\r\n\r\n";
+
+            // Act
+            string msg = RunMSPC(template: template);
+
+            // Assert
+            Assert.Equal(expected, msg);
         }
     }
 }

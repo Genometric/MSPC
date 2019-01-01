3f32325d1891aac377c07cc9d018e65c98a208d2
VJalili <Jalili.vahid@gmail.com>
2018-12-31 18:59:38 -0800

    Update expected text in ShowHelpText unit test.

--- CLI.Tests/Main.cs
+++ CLI.Tests/Main.cs
@@ -260,18 +260,24 @@ namespace Genometric.MSPC.CLI.Tests
         {
             // Arrange
             string expected = 
-                "\r\n\r\nUsage:  [options]" +
-                "\r\n\r\nOptions:" +
-                "\r\n -? | -h | --help                      Show help information" +
-                "\r\n - i | --input < value > Input samples to be processed in Browser Extensible Data(BED) Format." +
-                "\r\n - p | --parser < value > Sets the path to the parser configuration file in JSON." +
-                "\r\n - r | --replicate < value > Sets the replicate type of samples.Possible values are: { Bio, Biological, Tec, Technical }" +
-                "\r\n - s | --tauS < value > Sets stringency threshold. All peaks with p-values lower than this value are considered as stringent peaks." +
-                "\r\n - w | --tauW < value > Sets weak threshold. All peaks with p-values higher than this value are considered as weak peaks." +
-                "\r\n - g | --gamma < value > Sets combined stringency threshold.The peaks with their combined p-values satisfying this threshold will be confirmed." +
-                "\r\n - a | --alpha < value > Sets false discovery rate of Benjamini–Hochberg step-up procedure.\r\n - c < value > Sets minimum number of overlapping peaks before combining p-values." +
-                "\r\n - m | --multipleIntersections < value > When multiple peaks from a sample overlap with a given peak, this argument defines which of the peaks to be considered: the one with lowest p - value, or the one with highest p-value ? Possible values are: { Lowest, Highest }" +
-                "\r\n\r\n";
+                " Version 15.8.0" +
+                "\r\n\r\nUsage: MSPC CLI [options]" +
+                "\r\n\r\nOptions:\r\n  -? | -h | --help                      Show help information" +
+                "\r\n  -v | --version                        Show version information" +
+                "\r\n  -i | --input <value>                  Input samples to be processed in Browser Extensible Data (BED) Format." +
+                "\r\n  -r | --replicate <value>              Sets the replicate type of samples. Possible values are: { Bio, Biological, Tec, Technical }" +
+                "\r\n  -w | --tauW <value>                   Sets weak threshold. All peaks with p-values higher than this value are considered as weak peaks." +
+                "\r\n  -s | --tauS <value>                   Sets stringency threshold. All peaks with p-values lower than this value are considered as stringent peaks." +
+                "\r\n  -g | --gamma <value>                  Sets combined stringency threshold. The peaks with their combined p-values satisfying this threshold will be confirmed." +
+                "\r\n  -a | --alpha <value>                  Sets false discovery rate of Benjamini–Hochberg step-up procedure." +
+                "\r\n  -c <value>                            Sets minimum number of overlapping peaks before combining p-values." +
+                "\r\n  -m | --multipleIntersections <value>  When multiple peaks from a sample overlap with a given peak, this argument defines which of the peaks to be " +
+                "considered: the one with lowest p-value, or the one with highest p-value? Possible values are: { Lowest, Highest }" +
+                "\r\n  -p | --parser <value>                 Sets the path to the parser configuration file in JSON." +
+                "\r\n\n\rDocumentation:\thttps://genometric.github.io/MSPC/" +
+                "\n\rSource Code:\thttps://github.com/Genometric/MSPC" +
+                "\n\rPublications:\thttps://genometric.github.io/MSPC/publications" +
+                "\n\r\r\n";
 
             // Act
             string msg = RunMSPC(template: template);

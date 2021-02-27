9c54e1baa4696d5b5000c688513f9fb82630df2c
vjalili <VJalili@users.noreply.github.com>
2021-02-26 18:01:04 -0800

    Add create_packages.sh.

---
+++ create_packages.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+Compress()
+{
+  DIR=$1
+  FILENAME=$2
+
+  WD=$(pwd)
+  cd $DIR
+  zip -q -r ../$FILENAME *
+  cd $WD
+  rm -rf $DIR
+
+  # The following is not a good alternative as
+  # it flattens the hierarchies and errors out
+  # if two files with same name under different
+  # directories exist.
+  #zip -j -q -r $FILENAME $DIR
+}
+
+# ----------------------------
+# Make Self-contained packages
+# ----------------------------
+dotnet publish CLI/CLI.csproj --output ./packages/mspc --no-self-contained -p:UseAppHost=True
+Compress ./packages/mspc mspc.zip
+
+# ----------------------------
+# Make Self-contained packages
+# ----------------------------
+# Runtime Identifiers
+# See the following page for a complete list of Identifiers:
+# https://docs.microsoft.com/en-us/dotnet/core/rid-catalog
+for RID in "win-x64" "osx-x64" "linux-x64"
+do
+  dotnet publish CLI/CLI.csproj --output ./packages/$RID --self-contained true --runtime $RID -p:UseAppHost=True
+  Compress ./packages/$RID $RID.zip
+done

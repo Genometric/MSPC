e2c05469c7b4219dc15eae510031e5a1cd9a8d72
VJalili <VJalili@users.noreply.github.com>
2022-08-24 21:00:01 -0400

    Serialize results to csv files.

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -6,6 +6,9 @@
         {
             var vsString = args[0].Split(";");
             var dataDir = args[1];
+            var resultsFilename = Path.Join(dataDir, "mspc_versions_benchmarking_results.tsv");
+            using (var writer = new StreamWriter(resultsFilename))
+                writer.WriteLine(Result.GetHeader());
 
             var versions = new List<Version>();
             foreach (var v in vsString)
@@ -15,6 +18,10 @@
             {
                 var test = new PerformanceTest();
                 var results = await test.Test(dataDir, version);
+                using (var writer = new StreamWriter(resultsFilename))
+                    foreach (var result in results)
+                        writer.WriteLine(result.ToString());
+
             }
         }
     }
